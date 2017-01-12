7b795af53f1c4c79ebfdbd50cba897fa59566de7
ste-wilkinson <du_monde@hotmail.com>
2017-01-12 22:19:39 +0000

    fix asos

--- src/ThreadAndMirror/ProductsBundle/Service/Crawler/AsosCrawler.php
+++ src/ThreadAndMirror/ProductsBundle/Service/Crawler/AsosCrawler.php
@@ -4,107 +4,107 @@ namespace ThreadAndMirror\ProductsBundle\Service\Crawler;
 
 class AsosCrawler extends AbstractCrawler
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getPid(DomCrawler $crawler)
-	{
-		return $this->getValueFromElement($crawler, '#CatwalkInventoryId');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPid(DomCrawler $crawler)
+    {
+        return $this->getTextFromElement($crawler, '#product-details .product-code span');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getName(DomCrawler $crawler)
-	{
-		return $this->getTextFromElement($crawler, 'h1');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getName(DomCrawler $crawler)
+    {
+        return $this->getTextFromElement($crawler, 'h1');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getBrandName(DomCrawler $crawler) 
-	{
-		return $this->getTextFromElement($crawler, '#ctl00_ContentMainPage_productInfoPanel > a > strong', 1);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBrandName(DomCrawler $crawler)
+    {
+        return $this->getTextFromElement($crawler, '#product-details .product-description span a strong', 0);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getCategory(DomCrawler $crawler) 
-	{
-		return $this->getTextFromElement($crawler, '#ctl00_ContentMainPage_productInfoPanel a strong', 0);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCategory(DomCrawler $crawler)
+    {
+        return $this->getTextFromElement($crawler, '#product-details .product-description span a strong', 1);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getDescription(DomCrawler $crawler) 
-	{
-		return $this->getTextFromList($crawler, '#ctl00_ContentMainPage_productInfoPanel li');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDescription(DomCrawler $crawler)
+    {
+        return $this->getTextFromList($crawler, '#product-details .product-description ul li');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getNow(DomCrawler $crawler) 
-	{
-		return $this->getTextFromAlternatingElements($crawler, '.outlet-current-price', '.product_price span');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getNow(DomCrawler $crawler)
+    {
+        return $this->getTextFromElement($crawler, 'section script', 3);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getWas(DomCrawler $crawler) 
-	{
-		return $this->getTextFromAlternatingElements($crawler, '.product_rrp span', null);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getWas(DomCrawler $crawler)
+    {
+        return $this->getTextFromElement($crawler, 'section script', 3);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getImages(DomCrawler $crawler) 
-	{
-		return $this->getSrcFromList($crawler, '.productThumbnails img');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getImages(DomCrawler $crawler)
+    {
+        return $this->getSrcFromList($crawler, '#gallery-content .thumbnails li img');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getPortraits(DomCrawler $crawler) 
-	{
-		return $this->getSrcFromList($crawler, '.productThumbnails img');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPortraits(DomCrawler $crawler)
+    {
+        return $this->getSrcFromList($crawler, '#gallery-content .thumbnails li img');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getThumbnails(DomCrawler $crawler) 
-	{
-		return $this->getSrcFromList($crawler, '.productThumbnails img');
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getThumbnails(DomCrawler $crawler)
+    {
+        return $this->getSrcFromList($crawler, '#gallery-content .thumbnails li img');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getAvailableSizes(DomCrawler $crawler) 
-	{
-		return [];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAvailableSizes(DomCrawler $crawler)
+    {
+        return $this->getTextFromList($crawler, '#core-product .colour-size-select option');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getStyleWith(DomCrawler $crawler) 
-	{
-		return null;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getStockedSizes(DomCrawler $crawler)
+    {
+        return $this->getTextFromList($crawler, '#core-product .colour-size-select option:not([disabled])');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getCustomHeaders()
-	{
-		return ['Cookie' => 'asos=currencyid=1'];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCustomHeaders()
+    {
+        return ['Cookie' => 'asos=currencyid=1'];
+    }
 }
--- src/ThreadAndMirror/ProductsBundle/Service/Formatter/AsosFormatter.php
+++ src/ThreadAndMirror/ProductsBundle/Service/Formatter/AsosFormatter.php
@@ -6,139 +6,180 @@ use ThreadAndMirror\ProductsBundle\Entity\Product;
 
 class AsosFormatter extends AbstractFormatter
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupFeedName(Product $product)
-	{ 
-		$result = $this
-			->format($product->getName())
-			->remove('ASOS CURVE')
-			->remove('ASOS MATERNITY')
-			->remove('ASOS PETITE')
-			->remove('ASOS TALL')
-			->remove('ASOS WHITE')
-			->replace($product->getBrandName().' ', '')
-			->sheer(' - ', false)
-			->trim()
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupFeedName(Product $product)
+    {
+        $result = $this
+            ->format($product->getName())
+            ->remove('ASOS CURVE')
+            ->remove('ASOS MATERNITY')
+            ->remove('ASOS PETITE')
+            ->remove('ASOS TALL')
+            ->remove('ASOS WHITE')
+            ->replace($product->getBrandName() . ' ', '')
+            ->sheer(' - ', false)
+            ->trim()
+            ->result();
 
-		$product->setName($result);
-	}
+        $product->setName($result);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupFeedDescription(Product $product)
-	{ 
-		$result = $this
-			->format($product->getName())
-			->sheer('ABOUT')
-			->append('.')
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupFeedDescription(Product $product)
+    {
+        $result = $this
+            ->format($product->getName())
+            ->sheer('ABOUT')
+            ->append('.')
+            ->result();
 
-		$product->setDescription($result);
-	}
+        $product->setDescription($result);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupFeedPid(Product $product)
-	{
-		$result = $this
-			->format($product->getImages()[0])
-			->explode('/', 9)
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupFeedPid(Product $product)
+    {
+        $result = $this
+            ->format($product->getImages()[0])
+            ->explode('/', 9)
+            ->result();
 
-		$product->setDescription($result);
-	}
+        $product->setDescription($result);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupFeedImages(Product $product)
-	{ 
-		$product->setImages([$product->getImages()[0]]);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupFeedImages(Product $product)
+    {
+        $product->setImages([$product->getImages()[0]]);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupFeedPortraits(Product $product)
-	{
-		$product->setPortraits(array($product->getImages()[0]));
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupFeedPortraits(Product $product)
+    {
+        $product->setPortraits(array($product->getImages()[0]));
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupFeedThumbnails(Product $product)
-	{
-		$result = $this
-			->format($product->getImages()[0])
-			->replace('image1xl.jpg', 'image1s.jpg')
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupFeedThumbnails(Product $product)
+    {
+        $result = $this
+            ->format($product->getImages()[0])
+            ->replace('image1xl.jpg', 'image1s.jpg')
+            ->result();
 
-		$product->setThumbnails(array($result));
-	}
+        $product->setThumbnails(array($result));
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupCrawledName(Product $product)
-	{
-		$result = $this
-			->format($product->getName())
-			->remove('ASOS CURVE')
-			->remove('ASOS MATERNITY')
-			->remove('ASOS PETITE')
-			->remove('ASOS TALL')
-			->remove('ASOS WHITE')
-			->remove('ASOS')
-			->replace($product->getBrandName().' ', '')
-			->trim()
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledName(Product $product)
+    {
+        $result = $this
+            ->format($product->getName())
+            ->remove('ASOS CURVE')
+            ->remove('ASOS MATERNITY')
+            ->remove('ASOS PETITE')
+            ->remove('ASOS TALL')
+            ->remove('ASOS WHITE')
+            ->remove('ASOS')
+            ->replace($product->getBrandName() . ' ', '')
+            ->trim()
+            ->result();
 
-		$product->setName($result);
-	}
+        $product->setName($result);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupCrawledDescription(Product $product)
-	{
-		$result = $this
-			->format($product->getDescription())
-			->implode()
-			->append('.')
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledDescription(Product $product)
+    {
+        $result = $this
+            ->format($product->getDescription())
+            ->implode()
+            ->append('.')
+            ->result();
 
-		$product->setDescription($result);
-	}
+        $product->setDescription($result);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupCrawledImages(Product $product)
-	{
-		$result = $this
-			->format($product->getImages())
-			->replace('s.jpg', 'xl.jpg')
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledNow(Product $product)
+    {
+        $result = $this
+            ->format($product->getNow())
+            ->sheer('{"current":')
+            ->sheer(',"previous":', false)
+            ->result();
 
-		$product->setImages($result);
-	}
+        $product->setNow($result);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function cleanupCrawledPortraits(Product $product)
-	{
-		$result = $this
-			->format($product->getPortraits())
-			->replace('s.jpg', 'xxl.jpg')
-			->result();
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledWas(Product $product)
+    {
+        $result = $this
+            ->format($product->getWas())
+            ->sheer(',"previous":')
+            ->sheer(',"rrp":', false)
+            ->result();
 
-		$product->setPortraits($result);
-	}
+        $product->setWas($result);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledImages(Product $product)
+    {
+        $result = $this
+            ->format($product->getImages())
+            ->sheer('?', false)
+            ->result();
+
+        $product->setImages($result);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledPortraits(Product $product)
+    {
+        $result = $this
+            ->format($product->getPortraits())
+            ->sheer('?', false)
+            ->result();
+
+        $product->setPortraits($result);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function cleanupCrawledThumbnails(Product $product)
+    {
+        $result = $this
+            ->format($product->getThumbnails())
+            ->sheer('?', false)
+            ->result();
+
+        $product->setPortraits($result);
+    }
 }
